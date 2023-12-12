841b504e55627aada57e750e91f678f105ffb36b
bkadel1 <144166993+bkadel1@users.noreply.github.com>
2023-12-11 22:23:32 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const addTaskButton = document.getElementById("addTask");
     const taskList = document.getElementById("taskList");
 
-    // Load tasks from local storage on page load
+    
     function loadTasksFromLocalStorage() {
         const storedTasks = localStorage.getItem("tasks");
 
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Save tasks to local storage
+    
     function saveTasksToLocalStorage() {
         const tasks = [];
         const taskElements = taskList.querySelectorAll("li");
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }
 
-    // Function to add a new task to the list
+    
     function addTaskToList(taskText) {
         const li = document.createElement("li");
         li.innerHTML = `
@@ -40,35 +40,35 @@ document.addEventListener("DOMContentLoaded", function () {
         taskList.appendChild(li);
     }
 
-    // Function to add a new task
+
     function addNewTask() {
         const taskText = taskInput.value.trim();
         if (taskText !== "") {
             addTaskToList(taskText);
             taskInput.value = "";
 
-            // Save the updated task list to local storage
+            
             saveTasksToLocalStorage();
         }
     }
 
-    // Function to mark a task as completed
+    
     function completeTask(event) {
         event.target.closest("li").classList.toggle("completed");
 
-        // Save the updated task list to local storage
+        
         saveTasksToLocalStorage();
     }
 
-    // Function to delete a task
+    
     function deleteTask(event) {
         event.target.closest("li").remove();
 
-        // Save the updated task list to local storage
+        
         saveTasksToLocalStorage();
     }
 
-    // Event listeners
+    
     addTaskButton.addEventListener("click", addNewTask);
     taskInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
@@ -83,6 +83,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Load tasks from local storage on page load
+    
     loadTasksFromLocalStorage();
 });
